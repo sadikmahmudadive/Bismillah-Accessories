a480e3dfbaac6900f92d1831be3db2470b8198ee
Md. Sadik Mahmud Adive <siradive137@gmail.com>
2026-05-04 18:31:23 +0600

    chore: update next.config.ts to allow image optimization from all hostnames

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
+        hostname: "**",
       },
     ],
   },
